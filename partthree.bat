697eeab31704d1f884b32849053e51e2aa2c4de1
Elmo Dunk <121550765+elmodunk@users.noreply.github.com>
2023-04-08 17:19:12 +0800

    Update partthree.bat

--- partthree.bat
+++ partthree.bat
@@ -1,8 +1,8 @@
 @echo off
 echo run
-tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Cannot get an army tunnel, ensure Ngrok_Auth_token is right in settings> Secrets> secret archive. Perhaps your previous VM still runs: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 -n 5 >Nul & exit
+tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Cannot get an army tunnel, ensure Ngrok_Auth_token is right in settings> Secrets> secret archive. Perhaps your previous VM still runs: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 -n 10 >Nul & exit
 :check
-ping 127.0.0.1 -n 5 > nul
+ping 127.0.0.1 -n 10 > nul
 cls
 echo run
 goto check
